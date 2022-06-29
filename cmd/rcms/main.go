0405e922248215bedef48e20e1c5c12d48fc8cea
Stepan <stepan-pl@protonmail.com>
2022-06-29 05:40:16 +0300

    Добавил логи

--- cmd/rcms/main.go
+++ cmd/rcms/main.go
@@ -13,14 +13,17 @@ import (
 
 func main() {
 	// подгружаем файлы конфигурации
+	log.Println("start config")
 	if err := config.LoadConfig("./config"); err != nil {
 		panic(fmt.Errorf("invalid application configuration: %s", err))
 	}
 
 	fmt.Println(config.Config.ConfigVar)
 
+	log.Println("create router")
 	router := httprouter.New()
 
+	log.Println("register user handler")
 	handler := user.NewHandler()
 	handler.Register(router)
 
@@ -28,6 +31,8 @@ func main() {
 }
 
 func start(router *httprouter.Router) {
+	log.Println("start application")
+
 	listener, err := net.Listen("tcp", ":1234")
 	if err != nil {
 		panic(err)
