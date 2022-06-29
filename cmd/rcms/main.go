5404efafa29641aacfff0d9ed07fd4030de1dbf0
Stepan <stepan-pl@protonmail.com>
2022-06-29 05:01:34 +0300

    Написал запуск приложения на IP

--- cmd/rcms/main.go
+++ cmd/rcms/main.go
@@ -3,7 +3,17 @@ package main
 import (
 	"fmt"
 	"github.com/IT-Nick/go-project-cms/cmd/rcms/config"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net"
+	"net/http"
+	"time"
 )
+
+func IndexHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	name := params.ByName("name")
+	w.Write([]byte(fmt.Sprintf("Hellp %s", name)))
+}
 
 func main() {
 	// подгружаем файлы конфигурации
@@ -12,4 +22,20 @@ func main() {
 	}
 
 	fmt.Println(config.Config.ConfigVar)
+
+	router := httprouter.New()
+	router.GET("/:name", IndexHandler)
+
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		panic(err)
+	}
+
+	server := &http.Server{
+		Handler:      router,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatalln(server.Serve(listener))
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/IT-Nick/go-project-cms
 go 1.17
 
 require (
+	github.com/julienschmidt/httprouter v1.3.0
 	github.com/spf13/viper v1.12.0
 	github.com/stretchr/testify v1.7.2
 )
